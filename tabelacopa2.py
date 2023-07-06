359f278d63c7833b5c359beba6fed72e768a8479
nickersonff <nickerson.ferreira@ifrn.edu.br>
2023-07-06 09:48:23 -0300

    Tests

--- tabelacopa2.py
+++ tabelacopa2.py
@@ -62,67 +62,7 @@ teams_list = ['Argentina',
               'Ivory Coast',
               'Slovenia']
 
-elo = pd.read_csv('./content/Elo-Rating-2022.csv')
-ranklist = {}
-for rankval, team in enumerate(teams_list):
-    ranklist[team] = {'rank': rankval+1, 'elo-rating': 7}
 
-df = pd.read_csv('./content/WorldCupMatches.csv')
-# do = pd.read_csv('/content/fifa_ranking-2022-10-06.csv')
-do = pd.read_csv('./content/Elo-Rating.csv', encoding='latin-1')
-toto = df[(df['Year'] > 1991) & (df['Year'] < 2022)]
-
-
-countries = list(set(do['Team']))
-years = np.arange(1994, 2015, 4)
-ranks = {}
-
-""" ALTERAR A DIFERENÇA DO RANK ?? """
-
-for yy in years:
-    ranks[yy] = {}
-    # doy = do[((do['rank_date']>str(yy)) & (do['rank_date']<str(yy+1)))]
-    doy = do[(do['Year'] == yy)]
-
-    ccin = list(set(doy['Team']))
-    for cc in countries:
-        if cc in ccin:
-            ranks[yy][cc] = list(doy[doy['Team'] == cc]['Rating'])[0]
-            if (ranks[yy][cc] < param.MIN_RATING):
-                ranks[yy][cc] = param.MIN_RATING
-        else:
-            ranks[yy][cc] = param.MIN_RATING
-
-dif_rank = []
-dif_goal = []
-for yy in np.arange(1994, 2015, 4):
-    hteams = list(toto[toto['Year'] == float(yy)]['Home Team Name'])
-    ateams = list(toto[toto['Year'] == float(yy)]['Away Team Name'])
-    hgoal = list(toto[toto['Year'] == float(yy)]['Home Team Goals'])
-    agoal = list(toto[toto['Year'] == float(yy)]['Away Team Goals'])
-    for hh in hteams:
-        if hh not in ranks[yy]:
-            ranks[yy][hh] = param.MIN_RATING
-    for hh in ateams:
-        if hh not in ranks[yy]:
-            ranks[yy][hh] = param.MIN_RATING
-    hteams_rank = [ranks[yy][hh] for hh in hteams]
-    ateams_rank = [ranks[yy][hh] for hh in ateams]
-    dif_rank.extend([hh-aa for hh, aa in zip(hteams_rank, ateams_rank)])
-    dif_goal.extend([hh-aa for hh, aa in zip(hgoal, agoal)])
-
-""" FIM DA ALTERAÇÃO """
-
-
-for ii in range(len(dif_rank)):
-    dif_goal[ii] = -1 * dif_goal[ii]
-    if (dif_rank[ii] < 0):
-        dif_rank[ii] = -1 * dif_rank[ii]
-        dif_goal[ii] = -1 * dif_goal[ii]
-
-plt.plot(dif_rank, dif_goal, 'kx')
-
-plt.hist(dif_rank, bins=np.arange(0, 50, 5))
 
 xxx = np.arange(-4, 7200, 5)
 xxx[0] = 0
